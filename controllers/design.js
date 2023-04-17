9978f08d7aa20aa9e69cf705dd97f89c58d914b5
James Aules <james_aules@icloud.com>
2023-04-17 15:39:57 -0400

    first commit/design controller

---
+++ controllers/design.js
@@ -0,0 +1,24 @@
+const router = requiere('express').Router()
+
+router.get('/', (req, res) => {
+    let design = [
+        {
+        name: 'Flor Roja',
+        pic: '',
+        qualities:'',
+        also:''
+        },
+        {
+        name: 'Flor Roja',
+        pic: '',
+        qualities:'',
+        also:''
+        }
+    ]
+
+    res.render('design/index', {design})
+})
+
+
+
+module.exports = router
