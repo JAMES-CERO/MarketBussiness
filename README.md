ab7c832dae63576ab964af200b1dc7d4ebefaa1d
James Aules <james_aules@icloud.com>
2023-04-17 19:11:23 -0400

    readME MD

---
+++ README.md
@@ -0,0 +1,12 @@
+# Market Store 
+
+this project is built as a commercail webpage.
+
+## Features
+
+You will be able to surf betwwen the page
+buy & check the characteristics of each item
+
+
+
+
--- controllers/design.js
+++ controllers/design.js
@@ -1,20 +1,25 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    let design = [
-        {
-        name: 'Flor Roja',
-        pic: '',
-        qualities:'',
-        also:''
-        },
-        {
-        name: 'Flor Roja',
-        pic: '',
-        qualities:'',
-        also:''
-        }
-    ]
+    //GET route will eventually show a list of all places
+    res.send('GET /design')
+
+
+
+    // let design = [
+    //     {
+    //     name: 'Flor Roja',
+    //     pic: '',
+    //     qualities:'',
+    //     also:''
+    //     },
+    //     {
+    //     name: 'Flor Roja',
+    //     pic: '',
+    //     qualities:'',
+    //     also:''
+    //     }
+    // ]
 
     res.render('design/index', {design})
 })
